e2bad999d1d137a7c3f82d85bcab945e53df115c
varshithayempally <varshithayempally@gmail.com>
2025-01-30 21:40:46 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,15 @@
+public class Operations {
+    public static void main(String[] args) {
+        int a=18;
+        int b=27;
+        int sum=a+b;
+        System.out.println("sum="+sum);
+        int difference=a-b;
+        System.out.println("difference="+difference);
+        int product=a*b;
+        System.out.println("product="+product);
+        int quotient=a/b;
+        System.out.println("quotient="+quotient);
+    }
+    
+}
